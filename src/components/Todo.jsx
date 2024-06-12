f9499e5249d147ad874679cb5bd3ee7ec5a80fb5
Darian Sawali <darianaxel@gmail.com>
2024-06-11 17:44:04 -0700

    implementing localStorage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,7 @@ import todo_icon from '../assets/todo_icon.png'
 import TodoItem from './TodoItem'
 
 const Todo = () => {
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [] );
     const inputRef = useRef();
     const add = () => {
         const inputText = inputRef.current.value.trim();
@@ -42,7 +42,7 @@ const Todo = () => {
     };
 
     useEffect(()=>{
-        console.log(todoList);
+        localStorage.setItem("todos", JSON.stringify(todoList))
     }, [todoList]);
 
     return (
