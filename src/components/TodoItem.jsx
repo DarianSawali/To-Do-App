8db0a6ae676cce92a79adcd29698afd7064a8477
Darian Sawali <darianaxel@gmail.com>
2024-06-11 17:35:30 -0700

    tick toggle visual effect

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import tick from '../assets/tick.png'
-// import not_tick from '../assets/not_tick.png'
+import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
 
@@ -8,8 +8,9 @@ const TodoItem = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
-            <img className='w-7' src={tick} alt="tick" />
-            <p className='tex-slate-700 ml-4 text-[1rem]' >
+            <img src={isComplete ? tick : not_tick} className='w-7' alt="tick" />
+            <p className={`tex-slate-700 ml-4 text-[1rem] decoration-slate-500
+                ${isComplete ? "line-through" : ""}`}>
                 {text}</p>
         </div>
 
